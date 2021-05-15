ba1e5a7fd08d5d0f7b05706940883252c22c45ce
warmthdawn <warmth06@sina.com>
2021-05-15 23:22:11 +0800

    :new: 加了点注释
    STYLE: 加了点注释

--- core/src/main/kotlin/top/limbang/doctor/core/api/IHookProviderRegistry.kt
+++ core/src/main/kotlin/top/limbang/doctor/core/api/IHookProviderRegistry.kt
@@ -3,10 +3,13 @@ package top.limbang.doctor.core.api
 import top.limbang.doctor.core.api.plugin.PluginHookProvider
 
 /**
- *
+ * 提供已经注册钩子的查询接口
  * @author WarmthDawn
  * @since 2021-05-14
  */
 interface IHookProviderRegistry {
+    /**
+     * 查询注册的钩子
+     */
     fun <T, V : PluginHookProvider<T>> provider(provider: Class<V>): PluginHookProvider<T>
 }
--- core/src/main/kotlin/top/limbang/doctor/core/api/plugin/PluginHookProvider.kt
+++ core/src/main/kotlin/top/limbang/doctor/core/api/plugin/PluginHookProvider.kt
@@ -8,6 +8,14 @@ package top.limbang.doctor.core.api.plugin
 typealias Hook<T> = T.() -> Unit
 
 interface PluginHookProvider<T> {
+    /**
+     * 添加钩子
+     * [hook] 一个lambda表达式，表示对钩子提供的类型进行操作
+     */
     fun addHook(hook: Hook<T>)
+
+    /**
+     * 删除钩子
+     */
     fun delHook(hook: Hook<T>)
 }
