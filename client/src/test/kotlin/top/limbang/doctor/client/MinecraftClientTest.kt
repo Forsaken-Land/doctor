040d14f4fbecef729ffdd0a11f540ced63dfb1ee
limbang <495071565@qq.com>
2021-05-17 14:27:04 +0800

    :new: 修改一些东西 抽出工具类,未完成离线登录识别

--- client/src/test/kotlin/top/limbang/doctor/client/MinecraftClientTest.kt
+++ client/src/test/kotlin/top/limbang/doctor/client/MinecraftClientTest.kt
@@ -1,15 +1,23 @@
 package top.limbang.doctor.client
 
-import org.junit.Test
-
-class MinecraftClientTest {
-
-//    @Test
-
-}
 
 fun main() {
-    MinecraftClient().start("mc.blackyin.xyz", 524)
-//    MinecraftClient().start("localhost", 25565)
+//    val host = "localhost"
+//    val port = 25565
+    val host = "mc.blackyin.xyz"
+    val port = 524
+
+//    val pingJson = MinecraftClient.ping(host, port).get()
+//    println(AutoUtils.autoVersion(pingJson))
+//    println(AutoUtils.autoForgeVersion(pingJson))
+
+
+    MinecraftClient()
+        .user("tfgv852@qq.com","12345678")
+        .authServerUrl("https://skin.blackyin.xyz/api/yggdrasil/authserver")
+        .sessionServerUrl("https://skin.blackyin.xyz/api/yggdrasil/sessionserver")
+        .start(host,port)
 }
+
+
 
