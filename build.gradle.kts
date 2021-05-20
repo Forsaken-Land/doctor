b0f35dcd18ea6aecb93be38c95bb8291bb758423
Doctor_Yin <1921841421@qq.com>
2021-05-20 20:03:21 +0800

    FIX:修复CI

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,18 +9,7 @@ val projectVersion: String by project
 version = projectVersion
 
 repositories {
-    val CI_JOB_TOKEN: String by project
-    maven {
-        url = uri("https://git.blackyin.xyz:8443/api/v4/groups/10/-/packages/maven")
-        name = "GitLab"
-        credentials(HttpHeaderCredentials::class.java) {
-            name = "Job-Token"
-            value = System.getenv(CI_JOB_TOKEN)
-        }
-        authentication {
-            create<HttpHeaderAuthentication>("header")
-        }
-    }
+
     maven("https://maven.aliyun.com/repository/public")
     mavenCentral()
 }
@@ -47,23 +36,25 @@ subprojects {
         testImplementation("ch.qos.logback:logback-classic:1.2.3")
     }
 }
-//publishing {
-//    val gitLabPrivateToken: String by project
-//    publications {
-//        create<MavenPublication>("maven") {
-//            from(components["java"])
-//        }
-//    }
-//    repositories {
-//        maven(url = "https://git.blackyin.xyz:8443/api/v4/projects/30/packages/maven") {
-//            credentials(HttpHeaderCredentials::class.java) {
-//                name = "Private-Token"
-//                value = gitLabPrivateToken
-//            }
-//            authentication {
-//                create<HttpHeaderAuthentication>("header")
-//            }
-//        }
-//    }
-//}
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            from(components["java"])
+        }
+    }
+    repositories {
+        val CI_JOB_TOKEN: String by project
+        maven {
+            url = uri("https://git.blackyin.xyz:8443/api/v4/groups/10/-/packages/maven")
+            name = "GitLab"
+            credentials(HttpHeaderCredentials::class.java) {
+                name = "Job-Token"
+                value = System.getenv(CI_JOB_TOKEN)
+            }
+            authentication {
+                create<HttpHeaderAuthentication>("header")
+            }
+        }
+    }
+}
 
