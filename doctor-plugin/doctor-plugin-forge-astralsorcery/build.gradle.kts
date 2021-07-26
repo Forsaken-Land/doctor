aab3adee8897cd49e6e9f1200c0d201b91b86259
Doctor_Yin <1921841421@qq.com>
2021-07-27 04:16:40 +0800

    :heavy_minus_sign: 删除不必要的依赖

--- doctor-plugin/doctor-plugin-forge-astralsorcery/build.gradle.kts
+++ doctor-plugin/doctor-plugin-forge-astralsorcery/build.gradle.kts
@@ -1,7 +1,5 @@
 dependencies {
-    implementation("io.reactivex.rxjava3:rxkotlin:3.0.1")
     implementation(project(":doctor-plugin:doctor-plugin-forge-core"))
-    implementation("com.github.Querz:NBT:6.1")
     implementation("io.netty:netty-all:4.1.56.Final")
     implementation(project(":doctor-client"))
 }
