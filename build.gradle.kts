027ad5ec3371598a51e5730bd9459678fb93e6df
Doctor_Yin <1921841421@qq.com>
2022-06-20 00:52:23 +0800

    :rocket: 发布仓库切换到中心仓库

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,12 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 plugins {
     val kotlinVersion = "1.6.21"
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.serialization") version kotlinVersion
     id("maven-publish")
     id("application")
+    id("signing")
 }
 
 allprojects {
@@ -14,6 +17,7 @@ allprojects {
     apply {
         plugin("org.gradle.maven-publish")
         plugin("application")
+        plugin("signing")
     }
 
     repositories {
@@ -55,23 +59,99 @@ allprojects {
     }
     publishing {
         publications {
-            create<MavenPublication>("maven") {
-                from(components["java"])
+            create<MavenPublication>("mavenJava") {
                 artifactId = project.name
-            }
-        }
-        repositories {
-            val user: String by project
-            val passwd: String by project
-            val mavenUrl: String by project
-            maven {
-                url = uri(mavenUrl)
-                credentials {
-                    username = user
-                    password = passwd
-                    isAllowInsecureProtocol = true
+                from(components["java"])
+                pom {
+                    url.set("https://github.com/Forsaken-Land/doctor")
+                    name.set(project.name)
+                    packaging = "jar"
+                    description.set("doctor一个简单的Minecraft库")
+
+                    scm {
+                        url.set("https://github.com/Forsaken-Land/doctor")
+                        connection.set("scm:git:git://github.com/Forsaken-Land/doctor")
+                        developerConnection.set("scm:git:ssh://github.com/Forsaken-Land")
+                    }
+                    licenses {
+                        license {
+                            name.set("GNU General Public License v3.0")
+                            url.set("https://www.gnu.org/licenses/gpl-3.0.html")
+                        }
+                    }
+                    developers {
+                        developer {
+                            id.set("Doctor_Yin")
+                            name.set("Doctor_Yin")
+                            email.set("1921841421@qq.com")
+                        }
+                        developer {
+                            id.set("limbang")
+                            name.set("limbang")
+                            email.set("495071565@qq.com")
+                        }
+                        developer {
+                            id.set("WarmthDawn")
+                            name.set("WarmthDawn")
+                            email.set("1304793916@qq.com")
+                        }
+                    }
                 }
             }
         }
+
+        repositories {
+            maven {
+                name = "OSSRH"
+                url = uri(
+                    if (project.version.toString()
+                            .endsWith("-SNAPSHOT")
+                    ) "https://s01.oss.sonatype.org/content/repositories/snapshots"
+                    else "https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"
+                )
+                val user: String by project
+                val passwd: String by project
+                credentials {
+                    username = user
+                    password = passwd
+                }
+            }
+        }
+
+//        publications {
+//            create<MavenPublication>("maven") {
+//                from(components["java"])
+//                artifactId = project.name
+//            }
+//        }
+//        repositories {
+//            val user: String by project
+//            val passwd: String by project
+//            val mavenUrl: String by project
+//            maven {
+//                url = uri(mavenUrl)
+//                credentials {
+//                    username = user
+//                    password = passwd
+//                    isAllowInsecureProtocol = true
+//                }
+//            }
+//        }
+    }
+    signing {
+        val signingKeyId: String by project
+        val signingKey: String by project
+        val signingPassword: String by project
+        useInMemoryPgpKeys(signingKeyId, signingKey, signingPassword)
+        sign(publishing.publications["mavenJava"])
+    }
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "1.8"
+}
+tasks.javadoc {
+    if (JavaVersion.current().isJava9Compatible) {
+        (options as StandardJavadocDocletOptions).addBooleanOption("html5", true)
     }
 }
